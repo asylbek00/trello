e0b4fed53c7fd329b3b77faf46239f43e2ce5aba
Asylbek <asylbek310800@gmail.com>
2022-12-06 17:08:02 +0600

    add

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import {  HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "store";
-import './firebase'
-
+import "./firebase";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <Provider store={store}>
-    <HashRouter basename="/">
+    <HashRouter >
       <App />
     </HashRouter>
   </Provider>
